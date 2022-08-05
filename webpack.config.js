97204a3ed9384adc43ae61f04520572373fabb61
Jamez Mayke <jamezmayke@gmail.com>
2022-08-04 22:29:26 -0300

    Adicionando arquivo webpack de configurações do projeto

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const PATH = require('path');
+const HTML_WEBPACK_PLUGIN = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+    entry: PATH.resolve(__dirname, 'src', 'index.jsx'),
+    output: {
+        path: PATH.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.jsx',
+        ]
+    },
+    devServer: {
+        static: PATH.resolve(__dirname, 'public'),
+        hot: true,
+    },
+    plugins: [
+        isDevelopment && new ReactRefreshWebpackPlugin(),
+        new HTML_WEBPACK_PLUGIN({
+            template: PATH.resolve(__dirname, 'public', 'index.html')
+        })
+    ].filter(Boolean),
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    } 
+                }
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            }
+        ]
+    }
+}
